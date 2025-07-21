5d91d916ff2dd5c094fdaac14266882f1200f34c
satvik-vinoth <satvikvinoth272@gmail.com>
2025-07-21 15:12:49 +0530

    path

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -2,7 +2,9 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
   issuer: 'https://accounts.google.com',
-  redirectUri: window.location.origin,
+  redirectUri: window.location.origin.includes('github.io')
+  ? 'https://satvik-vinoth.github.io/email-organizer/'
+  : window.location.origin,
   clientId: '601461228422-juptkj18s6lm70tfn9lo2od9fbrmelj3.apps.googleusercontent.com',
   scope: 'openid profile email https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.modify https://www.googleapis.com/auth/gmail.send',
   strictDiscoveryDocumentValidation: false,
